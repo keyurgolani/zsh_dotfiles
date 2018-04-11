dbdd3107909a8594e57994cb29eace49fc6acc09
Keyur Golani <keyurrgolani@gmail.com>
2018-04-10 21:46:48 -0700

    Adding Some Utility Shell Functions

---
+++ zsh/zsh_functions.sh
@@ -0,0 +1,61 @@
+mcd () { mkdir -p "$1" && cd "$1"; }
+cd() { builtin cd "$@"; ls; }
+function extract() {
+  if [ -f "$1" ] ; then
+    local filename=$(basename "$1")
+    local foldername="${filename%%.*}"
+    local fullpath=`perl -e 'use Cwd "abs_path";print abs_path(shift)' "$1"`
+    local didfolderexist=false
+    if [ -d "$foldername" ]; then
+      didfolderexist=true
+      read -p "$foldername already exists, do you want to overwrite it? (y/n) " -n 1
+      echo
+      if [[ $REPLY =~ ^[Nn]$ ]]; then
+        return
+      fi
+    fi
+    mkdir -p "$foldername" && cd "$foldername"
+    case $1 in
+      *.tar.bz2) tar xjf "$fullpath" ;;
+      *.tar.gz) tar xzf "$fullpath" ;;
+      *.tar.xz) tar Jxvf "$fullpath" ;;
+      *.tar.Z) tar xzf "$fullpath" ;;
+      *.tar) tar xf "$fullpath" ;;
+      *.taz) tar xzf "$fullpath" ;;
+      *.tb2) tar xjf "$fullpath" ;;
+      *.tbz) tar xjf "$fullpath" ;;
+      *.tbz2) tar xjf "$fullpath" ;;
+      *.tgz) tar xzf "$fullpath" ;;
+      *.txz) tar Jxvf "$fullpath" ;;
+      *.zip) unzip "$fullpath" ;;
+      *) echo "'$1' cannot be extracted via extract()" && cd .. && ! $didfolderexist && rm -r "$foldername" ;;
+    esac
+  else
+    echo "'$1' is not a valid file"
+  fi
+}
+
+up() {
+  str=""
+  count=0
+  while [ "$count" -lt "$1" ];
+  do
+    str=$str"../"
+    let count=count+1
+  done
+  cd $str
+}
+
+# do sudo, or sudo the last command if no argument given
+function s() {
+    if [[ $# == 0 ]]; then
+        sudo $(history -p '!!')
+    else
+        sudo "$@"
+    fi
+}
+
+# Validate JSON file with `cat jsonfile.json | validateJSON`
+function validateJSON() {
+  python -m json.tool  >> /dev/null && echo "Valid JSON" || echo "NOT valid JSON";
+}
--- zsh/zshrc_manager.sh
+++ zsh/zshrc_manager.sh
@@ -11,3 +11,4 @@ echo "Updating configuration"
 #(cd ~/dotfiles && time_out 3 git pull && time_out 3 git submodule update --init --recursive)
 (cd ~/dotfiles && git pull && git submodule update --init --recursive)
 source ~/dotfiles/zsh/zshrc.sh
+source ~/dotfiles/zsh/zsh_functions.sh
